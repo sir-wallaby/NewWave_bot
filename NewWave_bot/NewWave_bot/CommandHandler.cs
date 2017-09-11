7419b3dd12befc9d183127f3277daca13dcfaa70
Wesley Taylor <wesley.taylor303@gmail.com>
2017-09-11 08:12:53 -0400

    initial commit. Loads skeleton of bot for Discord.net 1.0x

---
+++ NewWave_bot/NewWave_bot/CommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NewWave_bot
+{
+
+    //creating the command handler class that
+    public class CommandHandler
+    {
+
+    }
+}
---
+++ NewWave_bot/NewWave_bot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace NewWave_bot
+{
+    class Program
+    {
+        private CommandService commands;
+        private DiscordSocketClient client;
+        private IServiceProvider services;
+
+        string token = "";
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+        
+        public async Task Start()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection().BuildServiceProvider();
+            
+            await InstallCommands(); //need to figure out how to define this method
+
+            await client.LoginAsync(TokenType.Bot, token);
+
+            await client.StartAsync();
+
+            client.Log += Log;
+
+            await Task.Delay(-1);
+        }
+        
+    }
+}
